3fa4ba2c7a0d7271179112fc6be0518a4cd2978c
yousirong <diziyong1523@gmail.com>
2024-07-24 13:31:52 +0900

    20240724

---
+++ model/model_onlyvalidation.py
@@ -0,0 +1,44 @@
+import os
+import torch
+from torch.utils.data import DataLoader
+from model import RFRNetModel
+from dataset import Dataset
+from torch.utils.tensorboard import SummaryWriter
+
+def load_model_and_evaluate(pth_path, val_loader, writer, step):
+    model = RFRNetModel()
+    model.initialize_model(path=pth_path, train=False)
+    model.cuda()
+    val_loss = model.validate(val_loader)
+    writer.add_scalar("Validation/Loss", val_loss, step)
+    return val_loss
+
+def evaluate_all_models(directory, val_loader, log_dir):
+    writer = SummaryWriter(log_dir)
+    pth_files = [f for f in os.listdir(directory) if f.endswith('.pth')]
+    pth_files.sort(key=lambda x: int(x.split('_')[1].split('.')[0]))  # Sort by the number in the filename
+    pth_files = [f for f in pth_files if int(f.split('_')[1].split('.')[0]) % 100 == 0]  # Select files with increments of 100
+
+    for pth_file in pth_files:
+        step = int(pth_file.split('_')[1].split('.')[0])
+        pth_path = os.path.join(directory, pth_file)
+        val_loss = load_model_and_evaluate(pth_path, val_loader, writer, step)
+        print(f"Validation Loss for {pth_file}: {val_loss:.4f}")
+    
+    writer.close()
+
+def create_val_loader(data_root, mask_root, mask_mode, target_size, batch_size=32, num_workers=4):
+    val_dataset = Dataset(data_root, mask_root, mask_mode, target_size, mask_reverse=False, training=False)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    return val_loader
+
+# Example usage
+directory = 'model/models/ust_chl_3'  # Update this to the path of your .pth files directory
+data_root = '/media/juneyonglee/My Book/data/Chl-a/train/perfect'  # Update this to the path of your validation data
+mask_root = '/media/juneyonglee/My Book/data/mask/Train'  # Update this to the path of your validation masks
+mask_mode = 0  # Update this to the specific mask mode
+target_size = (256, 256)  # Update this to your target size
+log_dir = 'model_save_path: model/models_validation/'  # Update this to your TensorBoard log directory
+
+val_loader = create_val_loader(data_root, mask_root, mask_mode, target_size)
+evaluate_all_models(directory, val_loader, log_dir)
